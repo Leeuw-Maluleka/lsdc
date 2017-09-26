28bcc3f85488dd431742ee7c12b08743f233f5f9
leeuwtshepomaluleka@gmail.com <leeuwtshepomaluleka@gmail.com>
2017-09-26 11:55:17 -0700

    scripts.js for studentsassessmentmarks

---
+++ scripts/scripts.js
@@ -0,0 +1,9 @@
+function validString(str) 
+{
+    if ((str.length == 0) || (str == 'none'))
+        {return false}
+    else
+        {return true}
+}
+function checktasformvalues(form)
+{alert("Input cannot be empty")}
