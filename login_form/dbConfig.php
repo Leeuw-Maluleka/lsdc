2cb535b7461607c3750f1efaedf37657df6d2bc0
kmolema@gmail.com <kmolema@gmail.com>
2017-10-11 21:08:56 +0200

    Fix errors to connect to the DB

--- login_form/dbConfig.php
+++ login_form/dbConfig.php
@@ -1,9 +1,9 @@
 <?php
 define ("DB_HOST", "localhost"); // set database host
-define ("DB_USER", "admin"); // set database user
-define ("DB_PASS","admin123"); // set database password
+define ("DB_USER", "lsdccoza_admin"); // set database user
+define ("DB_PASS","lsdcadmin123"); // set database password
 define ("DB_NAME","lsdccoza_lsdcdb"); // set database name
 
-$link = mysql_connect(localhost, admin, admin123) or die("Couldn't make connection.");
-$db = mysql_select_db(lsdccoza_lsdcdb, $link) or die("Couldn't select database");
+$link = mysql_connect(DB_HOST, DB_USER, DB_PASS) or die("Couldn't make connection.");
+$db = mysql_select_db(DB_NAME, $link) or die("Couldn't select database");
 ?>
