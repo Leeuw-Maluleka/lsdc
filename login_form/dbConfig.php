1f8b258eeb4c5a618dd89f6a77620522f1c63734
leeuwtshepomaluleka@gmail.com <leeuwtshepomaluleka@gmail.com>
2017-10-11 10:35:42 -0700

    lsdc login_form scripts

---
+++ login_form/dbConfig.php
@@ -0,0 +1,9 @@
+<?php
+define ("DB_HOST", "localhost"); // set database host
+define ("DB_USER", "admin"); // set database user
+define ("DB_PASS","admin123"); // set database password
+define ("DB_NAME","lsdccoza_lsdcdb"); // set database name
+
+$link = mysql_connect(localhost, admin, admin123) or die("Couldn't make connection.");
+$db = mysql_select_db(lsdccoza_lsdcdb, $link) or die("Couldn't select database");
+?>
