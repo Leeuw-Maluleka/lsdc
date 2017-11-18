897a7b0a8b0f75c9b2814a5f9e01d7c58a1096f5
kmolema@gmail.com <kmolema@gmail.com>
2017-11-18 20:04:06 +0200

    DB script update

--- Resources/lsdccoza_lsdcdb.sql
+++ Resources/lsdccoza_lsdcdb.sql
@@ -1,38 +1,30 @@
- Server: localhost  -   Database: lsdcdb
- 
--- phpMyAdmin SQL Dump
--- version 3.2.0.1
--- http://www.phpmyadmin.net
---
--- Host: localhost
--- Generation Time: Oct 11, 2017 at 04:01 PM
--- Server version: 5.1.36
--- PHP Version: 5.3.0
 
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-
---
--- Database: `lsdcdb`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `login`
---
-
-CREATE TABLE IF NOT EXISTS `login` (
+CREATE TABLE IF NOT EXISTS `coursefees` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(50) NOT NULL,
-  `password` varchar(50) NOT NULL,
-  `checkbox` varchar(50) NOT NULL,
+  `CourseCode` varchar(10) NOT NULL,
+  `Payment` decimal(6,2) NOT NULL,
+  `Registration` decimal(6,2) DEFAULT NULL,
+  `Certification` decimal(6,2) DEFAULT NULL,
+  `CarryCard` decimal(6,2) DEFAULT NULL,
+  `Duration` int(11) NOT NULL,
+  `Timestamp` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`ID`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=41 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=13 ;
 
 --
--- Dumping data for table `login`
+-- Dumping data for table `coursefees`
 --
-
+DELETE FROM coursefees;
+INSERT INTO `coursefees` (`ID`, `CourseCode`, `Payment`, `Registration`, `Certification`, `CarryCard`, `Duration`, `Timestamp`) VALUES
+(4, 'ECD', '570.00', '1200.00', '1000.00', NULL, 11, '2017-11-15 18:28:13'),
+(5, 'EUC', '570.00', '1200.00', '1000.00', NULL, 11, '2017-11-15 18:37:33'),
+(6, 'TS', '650.00', '1200.00', '1000.00', NULL, 11, '2017-11-15 18:37:33'),
+(7, 'ECD', '570.00', '1200.00', '1000.00', NULL, 11, '2017-11-15 18:37:34'),
+(8, 'WRO', '640.00', '1000.00', '1000.00', NULL, 11, '2017-11-15 18:37:34'),
+(9, 'SD', '650.00', '1200.00', '1000.00', NULL, 11, '2017-11-15 18:37:34'),
+(10, 'LD', '910.00', '1500.00', '1000.00', NULL, 11, '2017-11-15 18:37:34'),
+(11, 'SE', '910.00', '1500.00', '1000.00', NULL, 11, '2017-11-15 18:37:34'),
+(12, 'ADV', '910.00', '1500.00', '1000.00', NULL, 11, '2017-11-15 18:37:34');
 
 -- --------------------------------------------------------
 
@@ -43,56 +35,124 @@ CREATE TABLE IF NOT EXISTS `login` (
 CREATE TABLE IF NOT EXISTS `lookuptable` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
   `Type` varchar(10) NOT NULL,
+  `ParentCode` varchar(10) DEFAULT NULL,
   `Code` varchar(10) NOT NULL,
   `Description` varchar(50) NOT NULL,
+  `Timestamp` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`ID`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=41 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=50 ;
 
 --
 -- Dumping data for table `lookuptable`
 --
+DELETE FROM lookuptable;
+INSERT INTO `lookuptable` (`ID`, `Type`, `ParentCode`, `Code`, `Description`, `Timestamp`) VALUES
+(1, 'Course', NULL, 'AD', 'Office Administration', '2017-11-15 17:56:04'),
+(2, 'Course', NULL, 'SYD', 'Systems Development', '2017-11-15 17:56:04'),
+(3, 'Cert', NULL, 'NT', 'National Certificate', '2017-11-15 17:56:04'),
+(4, 'Cert', NULL, 'CT', 'Certificate', '2017-11-15 17:56:04'),
+(5, 'Course', 'NT', 'TS', 'Technical Support', '2017-11-15 17:56:04'),
+(6, 'Course', NULL, 'FO', 'Forklift Operation', '2017-11-15 17:56:04'),
+(7, 'Course', NULL, 'KO', 'Krane Operator', '2017-11-15 17:56:04'),
+(8, 'Course', NULL, 'REC', 'Receptionist', '2017-11-15 17:56:04'),
+(9, 'Course', NULL, 'CL', 'Computer Literacy', '2017-11-15 17:56:04'),
+(10, 'Course', NULL, 'CC', 'Call Centre', '2017-11-15 17:56:04'),
+(11, 'Course', NULL, 'WR', 'Wholesale and Retail', '2017-11-15 17:56:04'),
+(12, 'Course', NULL, 'OT', 'Other', '2017-11-15 17:56:04'),
+(13, 'Ach', NULL, 'P', 'Pass', '2017-11-15 17:56:04'),
+(14, 'Ach', NULL, 'F', 'Fail', '2017-11-15 17:56:04'),
+(15, 'Modules', NULL, 'IC', 'Introduction to Computers', '2017-11-15 17:56:04'),
+(16, 'Modules', NULL, 'ME', 'Maintaining Office Equipment', '2017-11-15 17:56:04'),
+(17, 'Modules', NULL, 'CN', 'Principles of Computer Networks', '2017-11-15 17:56:04'),
+(18, 'Modules', NULL, 'CA', 'Computer Architecture', '2017-11-15 17:56:04'),
+(19, 'Modules', NULL, 'PRF', 'Present Information In Report Formats', '2017-11-15 17:56:04'),
+(20, 'Modules', NULL, 'WPST', 'Write/Presents/Sign Texts', '2017-11-15 17:56:04'),
+(21, 'Modules', NULL, 'ICT', 'ICT Risks', '2017-11-15 17:56:04'),
+(22, 'Modules', NULL, 'LC', 'Language & Communication in Occupational Learning', '2017-11-15 17:56:04'),
+(23, 'Modules', NULL, 'OSC', 'Oral & Sign Communication', '2017-11-15 17:56:04'),
+(24, 'Modules', NULL, 'PR', 'Presentation', '2017-11-15 17:56:04'),
+(25, 'Modules', NULL, 'SP', 'Spreadsheet', '2017-11-15 17:56:04'),
+(26, 'Modules', NULL, 'WP', 'Word Processing', '2017-11-15 17:56:04'),
+(27, 'SAQA', NULL, '7785', '7785', '2017-11-15 17:56:04'),
+(28, 'SAQA', NULL, '117925', '117925', '2017-11-15 17:56:04'),
+(29, 'SAQA', NULL, '13931', '13931', '2017-11-15 17:56:04'),
+(30, 'SAQA', NULL, '14913', '14913', '2017-11-15 17:56:04'),
+(31, 'SAQA', NULL, '14917', '14917', '2017-11-15 17:56:04'),
+(32, 'SAQA', NULL, '110023', '110023', '2017-11-15 17:56:04'),
+(33, 'SAQA', NULL, '116945', '116945', '2017-11-15 17:56:04'),
+(34, 'SAQA', NULL, '117926', '117926', '2017-11-15 17:56:04'),
+(35, 'SAQA', NULL, '119467', '119467', '2017-11-15 17:56:04'),
+(36, 'SAQA', NULL, '119472', '119472', '2017-11-15 17:56:04'),
+(37, 'SAQA', NULL, '116930', '116930', '2017-11-15 17:56:04'),
+(38, 'SAQA', NULL, '116943', '116943', '2017-11-15 17:56:04'),
+(39, 'SAQA', NULL, '116942', '116942', '2017-11-15 17:56:04'),
+(40, 'SAQA', NULL, '119078', '119078', '2017-11-15 17:56:04'),
+(41, 'Cert', NULL, 'ST', 'Short Course', '2017-11-15 17:56:04'),
+(43, 'Course', 'NT', 'EUC', 'End User Computing (NQF L3)', '2017-11-15 17:56:04'),
+(44, 'Course', 'NT', 'ECD', 'Early Childhood Development (L4)', '2017-11-15 17:56:04'),
+(45, 'Course', 'NT', 'WRO', 'Wholesale & Retail Operations (L3)', '2017-11-15 17:56:04'),
+(46, 'Course', 'NT', 'SWD', 'Software Development (L4 / L5)', '2017-11-15 17:56:04'),
+(47, 'Course', 'NT', 'LD', 'Leadership Development (NQF L4)', '2017-11-15 17:56:04'),
+(48, 'Course', 'NT', 'SE', 'Sound Engineering (NQF L5)', '2017-11-15 17:56:04'),
+(49, 'Course', 'NT', 'ADV', 'Advertising (NQF L5)', '2017-11-15 17:56:04');
 
-INSERT INTO `lookuptable` (`ID`, `Type`, `Code`, `Description`) VALUES
-(1, 'Course', 'AD', 'Office Administration'),
-(2, 'Course', 'SD', 'Systems Development'),
-(3, 'Cert', 'NT', 'National Certificate'),
-(4, 'Cert', 'CT', 'Certificate'),
-(5, 'Course', 'TS', 'Technical Support'),
-(6, 'Course', 'FO', 'Forklift Operation'),
-(7, 'Course', 'KO', 'Krane Operator'),
-(8, 'Course', 'REC', 'Receptionist'),
-(9, 'Course', 'CL', 'Computer Literacy'),
-(10, 'Course', 'CC', 'Call Centre'),
-(11, 'Course', 'WR', 'Wholesale and Retail'),
-(12, 'Course', 'OT', 'Other'),
-(13, 'Ach', 'P', 'Pass'),
-(14, 'Ach', 'F', 'Fail'),
-(15, 'Modules', 'IC', 'Introduction to Computers'),
-(16, 'Modules', 'ME', 'Maintaining Office Equipment'),
-(17, 'Modules', 'CN', 'Principles of Computer Networks'),
-(18, 'Modules', 'CA', 'Computer Architecture'),
-(19, 'Modules', 'PRF', 'Present Information In Report Formats'),
-(20, 'Modules', 'WPST', 'Write/Presents/Sign Texts'),
-(21, 'Modules', 'ICT', 'ICT Risks'),
-(22, 'Modules', 'LC', 'Language & Communication in Occupational Learning'),
-(23, 'Modules', 'OSC', 'Oral & Sign Communication'),
-(24, 'Modules', 'PR', 'Presentation'),
-(25, 'Modules', 'SP', 'Spreadsheet'),
-(26, 'Modules', 'WP', 'Word Processing'),
-(27, 'SAQA', '7785', '7785'),
-(28, 'SAQA', '117925', '117925'),
-(29, 'SAQA', '13931', '13931'),
-(30, 'SAQA', '14913', '14913'),
-(31, 'SAQA', '14917', '14917'),
-(32, 'SAQA', '110023', '110023'),
-(33, 'SAQA', '116945', '116945'),
-(34, 'SAQA', '117926', '117926'),
-(35, 'SAQA', '119467', '119467'),
-(36, 'SAQA', '119472', '119472'),
-(37, 'SAQA', '116930', '116930'),
-(38, 'SAQA', '116943', '116943'),
-(39, 'SAQA', '116942', '116942'),
-(40, 'SAQA', '119078', '119078');
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `members`
+--
+
+CREATE TABLE IF NOT EXISTS `members` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(256) NOT NULL,
+  `nick_name` varchar(256) NOT NULL,
+  `email` varchar(256) NOT NULL,
+  `password` varchar(256) NOT NULL,
+  `Privileges` int(11) NOT NULL,
+  `Timestamp` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=9 ;
+
+--
+-- Dumping data for table `members`
+--
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `privileges`
+--
+
+CREATE TABLE IF NOT EXISTS `privileges` (
+  `ID` int(11) NOT NULL,
+  `Description` varchar(50) NOT NULL DEFAULT 'None'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `privileges`
+--
+DELETE FROM `privileges`;
+INSERT INTO `privileges` (`ID`, `Description`) VALUES
+(0, 'None'),
+(1, 'Admin'),
+(2, 'Guest');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `registrations`
+--
+
+CREATE TABLE IF NOT EXISTS `registrations` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `CourseCode` varchar(10) NOT NULL,
+  `MemberName` varchar(256) NOT NULL,
+  `Timestamp` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=17 ;
+
+--
+-- Dumping data for table `registrations`
+--
 
 -- --------------------------------------------------------
 
@@ -112,14 +172,7 @@ CREATE TABLE IF NOT EXISTS `studentpersonalinformation` (
   `StudentHomeAddress` varchar(40) NOT NULL,
   `StudentsContactDetails` bigint(10) NOT NULL,
   PRIMARY KEY (`studentpersonalinformationID`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `studentpersonalinformation`
---
-
-INSERT INTO `studentpersonalinformation` (`studentpersonalinformationID`, `StudentFirstName`, `StudentMiddleName`, `StudentLastName`, `StudentIDno`, `StudentAge`, `StudentOccupation`, `StudentGender`, `StudentHomeAddress`, `StudentsContactDetails`) VALUES
-(1, 'Don', 'John', 'Laka', '9901012223334', 18, 'Unemployed', 'Male', 'Plot 613 Bosplaas West', 815265598);
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -137,42 +190,13 @@ CREATE TABLE IF NOT EXISTS `studentsassessmentmarks` (
   `SAQAID` varchar(10) NOT NULL,
   `PercentageAchieved` decimal(5,2) NOT NULL,
   `Achievement` varchar(4) NOT NULL,
+  `Timestamp` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`studentassessmentmarksID`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1049 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1086 ;
 
 --
 -- Dumping data for table `studentsassessmentmarks`
 --
-
-INSERT INTO `studentsassessmentmarks` (`studentassessmentmarksID`, `StudentName`, `StudentIDno`, `CourseName`, `CertificateType`, `ModuleName`, `SAQAID`, `PercentageAchieved`, `Achievement`) VALUES
-(1022, 'Boipelo Maluleka', '9501234567891', 'AD', 'NT', 'IC', '117925', '88.00', ''),
-(1021, 'Leeuw Maluleka', '9004021548957', 'System Development', 'National Certificate', 'Architecture', '13931', '69.00', 'Pass'),
-(1023, 'Moeti Malesela', '8701234567890', 'TS', 'NT', 'WP', '116942', '83.00', 'P'),
-(1024, 'lebohang More', '990123456789', 'SD', 'NT', 'IC', '117925', '40.00', 'F'),
-(1025, 'lebohang More', '990123456789', 'SD', 'NT', 'IC', '117925', '40.00', 'F'),
-(1026, '', '', 'none', 'none', 'none', 'none', '0.00', 'none'),
-(1027, '', '', 'none', 'none', 'none', 'none', '0.00', 'none'),
-(1028, '', '', 'none', 'none', 'none', 'none', '0.00', 'none'),
-(1029, '', '', 'none', 'none', 'none', 'none', '0.00', 'none'),
-(1030, '', '', 'none', 'none', 'none', 'none', '0.00', 'none'),
-(1031, '', '', 'none', 'none', 'none', 'none', '0.00', 'none'),
-(1032, '', '', 'none', 'none', 'none', 'none', '0.00', 'none'),
-(1033, '', '', 'none', 'none', 'none', 'none', '0.00', 'none'),
-(1034, '', '', 'none', 'none', 'none', 'none', '0.00', 'none'),
-(1035, '', '', 'none', 'none', 'none', 'none', '0.00', 'none'),
-(1036, '', '', 'none', 'none', 'none', 'none', '0.00', 'none'),
-(1037, '', '', 'none', 'none', 'none', 'none', '0.00', 'none'),
-(1038, '', '', 'none', 'none', 'none', 'none', '0.00', 'none'),
-(1039, '', '', 'none', 'none', 'none', 'none', '0.00', 'none'),
-(1040, '', '', 'none', 'none', 'none', 'none', '0.00', 'none'),
-(1041, '', '', 'none', 'none', 'none', 'none', '0.00', 'none'),
-(1042, '', '', 'none', 'none', 'none', 'none', '0.00', 'none'),
-(1043, '', '', 'none', 'none', 'none', 'none', '0.00', 'none'),
-(1044, '', '', 'none', 'none', 'none', 'none', '0.00', 'none'),
-(1045, 'Victor Maemane', '', 'none', 'none', 'none', 'none', '0.00', 'none'),
-(1046, 'Victor Maemane', '', 'none', 'none', 'none', 'none', '0.00', 'none'),
-(1047, 'Evodea Moeketsane', '8301234567890', 'none', 'none', 'none', 'none', '0.00', 'none'),
-(1048, 'Evodea Moeketsane', '8301234567890', 'none', 'none', 'none', 'none', '0.00', 'none');
 
 -- --------------------------------------------------------
 
@@ -192,10 +216,5 @@ CREATE TABLE IF NOT EXISTS `studentsonarrears` (
   `InterestPercentage` decimal(5,0) NOT NULL,
   `AmountDue` varchar(20) NOT NULL,
   PRIMARY KEY (`StudentsOnArrearsID`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `studentsonarrears`
---
-
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
